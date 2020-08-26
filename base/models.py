2f68a96ad750d1ad24173c4727f425c448a5f1bb
Nguyễn Tiến Anh Vương <34151449+nhockcukuin@users.noreply.github.com>
2020-08-26 07:15:08 +0700

    covid 19

---
+++ base/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200,null=True,blank=True)
+    #thumbnail
+    body = models.TextField(null=True,blank=True)
+    created = models.TimeField(auto_now_add=True)
+    activity = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True)
+    #slug = 
+    
+    def __str__(self):
+        return self.headline
+    
